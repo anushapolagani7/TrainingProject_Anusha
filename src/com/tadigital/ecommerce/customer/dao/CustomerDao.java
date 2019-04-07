35165245f4317f2824a9b07b2b431e1d3c5bb6a6
Anusha <anushapolagani7@gmail.com>
2019-04-07 16:30:44 +0530

    changed customer dao file

--- src/com/tadigital/ecommerce/customer/dao/CustomerDao.java
+++ src/com/tadigital/ecommerce/customer/dao/CustomerDao.java
@@ -4,12 +4,15 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import javax.servlet.http.HttpSession;
+
 import com.tadigital.ecommerce.customer.entity.Customer;
 
 public class CustomerDao extends Dao {
 	public boolean selectCustomerByEmailAndPassword(Customer customer) {
 		boolean status = false;
-		
+	
 		Connection con = openConnection();
 		Statement stmt = openStatement(con);
 		ResultSet rs = null;
@@ -61,7 +64,69 @@ public class CustomerDao extends Dao {
 		
 		return status;
 	}
-	
+	public boolean changeCustomerPassword(Customer customer) {
+boolean status = false;
+		
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+		
+		try {
+			String sql1="SELECT * FROM customer_info WHERE customer_email='"+customer.getEmail()+"' AND customer_pwd='"+customer.getOldPassword()+"'";
+			ResultSet rs=stmt.executeQuery(sql1);
+			while(rs.next())
+			{
+			
+				if(customer.getReNewPassword().equals(customer.getNewPassword())) {
+					
+				
+				String sql2 = "UPDATE  customer_info SET customer_pwd='"+customer.getNewPassword()+"' WHERE customer_email='"+customer.getEmail()+"'";
+				
+					int rows = stmt.executeUpdate(sql2);
+					if(rows != 0) {
+						status = true;
+					}
+				}
+				
+			}	
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		} finally {
+			closeStatement(stmt);
+			closeConnection(con);
+		}
+		
+		return status;
+		
+		
+	}
+	public boolean changeCustomerPersonalDetails(Customer customer) {
+		boolean status = false;
+				
+				Connection con = openConnection();
+				Statement stmt = openStatement(con);
+				
+				try {
+					String sql1="UPDATE customer_info SET customer_fname='"+customer.getFirstName()+"',customer_lname='"+customer.getLastName()+"',customer_gender='"+customer.getGender()+"',customer_country='"+customer.getCountry()+"',customer_state='"+customer.getState()+"',customer_address='"+customer.getAddress()+"',customer_contact='"+customer.getContact()+"',customer_email='"+customer.getNEmail()+"' WHERE customer_email='"+customer.getEmail()+"'";
+					
+						
+							int rows = stmt.executeUpdate(sql1);
+							if(rows != 0) {
+								status = true;
+							}
+						
+						
+				} catch (SQLException sqle) {
+					sqle.printStackTrace();
+				} finally {
+					closeStatement(stmt);
+					closeConnection(con);
+				}
+				
+				return status;
+				
+				
+			}
+			
 	
 
 
